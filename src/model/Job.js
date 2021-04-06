42f98d64c6822933559d8b79838c25732e4d4ddc
Daniel do Casal de Paula <daniel.c.paula@hotmail.com>
2021-04-05 22:08:09 -0300

    Aula 02 - Refatorado o Jobs e Profile

--- src/model/Job.js
+++ src/model/Job.js
@@ -1,16 +1,30 @@
-data: [
+let data = [
   {
     id: 1,
-    name: "Pizzaria guloso", 
-    "daily-hours": 2, 
+    name: "Pizzaria guloso",
+    "daily-hours": 2,
     "total-hours": 1,
-    "created_At" : Date.now(), //Atribuindo a data de hoje,
+    created_At: Date.now(), //Atribuindo a data de hoje,
   },
   {
     id: 2,
-    name: "OneTwo Project", 
-    "daily-hours": 3, 
+    name: "OneTwo Project",
+    "daily-hours": 3,
     "total-hours": 47,
-    "created_At" : Date.now(), //Atribuindo a data de hoje
+    created_At: Date.now(), //Atribuindo a data de hoje
+  },
+];
+
+module.exports = {
+  get() {
+    return data
+  },
+  
+  update(newJob) {
+    data = newJob
+  },
+
+  delete(id) {
+    data = data.filter(job => Number(job.id) !== Number(id))
   }
-]
+}
